b9208bafd938c25d7ad3aed31f72eed7655e0958
Awais Shabir <awais_shabir@hotmail.com>
2021-09-14 22:49:49 +0300

    Added Role Class

---
+++ User.Api/Data/Entities/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.Api.Data.Entities
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; private set; }
+        public Role()
+        {
+
+        }
+        public Role(string roleName)
+        {
+            this.RoleName = roleName;
+        }
+    }
+}
